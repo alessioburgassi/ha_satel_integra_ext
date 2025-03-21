285a5835e80c0738518e6d5ce4b2409db3644273
alessioburgassi <a.burgassi@gmail.com>
2025-03-21 21:12:38 +0100

    Update const.py

--- custom_components/satel_integra/const.py
+++ custom_components/satel_integra/const.py
@@ -2,7 +2,7 @@ import logging
 
 _LOGGER = logging.getLogger(__package__)
 
-DOMAIN = "satel_integra_ext"
+DOMAIN = "satel_integra"
 
 DEFAULT_ALARM_NAME = "satel_integra"
 DEFAULT_PORT = 7094
@@ -17,6 +17,13 @@ CONF_ARM_HOME_MODE = "arm_home_mode"
 CONF_ZONE_NAME = "name"
 CONF_ZONE_TYPE = "type"
 CONF_ZONES = "zones"
+CONF_ZONES_ALARM = "zones_alarm"
+CONF_ZONES_MEM_ALARM = "zones_mem_alarm"
+CONF_ZONES_TAMPER = "zones_tamper"
+CONF_ZONES_MEM_TAMPER = "zones_mem_tamper"
+CONF_ZONES_BYPASS = "zones_bypass"
+CONF_ZONES_MASKED = "zones_masked"
+CONF_ZONES_MEM_MASKED = "zones_mem_masked"
 CONF_OUTPUTS = "outputs"
 CONF_TEMP_SENSORS = "temperature_sensors"
 CONF_SWITCHABLE_OUTPUTS = "switchable_outputs"
@@ -27,5 +34,14 @@ SIGNAL_PANEL_MESSAGE = "satel_integra.panel_message"
 SIGNAL_PANEL_ARM_AWAY = "satel_integra.panel_arm_away"
 SIGNAL_PANEL_ARM_HOME = "satel_integra.panel_arm_home"
 SIGNAL_PANEL_DISARM = "satel_integra.panel_disarm"
-SIGNAL_ZONES_UPDATED = "satel_integra.zones_updated"
+
+SIGNAL_VIOLATED_UPDATED = "satel_integra.zones_updated"
+SIGNAL_ALARM_UPDATED = "satel_integra.zones_alarm"
+SIGNAL_MEM_ALARM_UPDATED = "satel_integra.zones_mem_alarm"
+SIGNAL_TAMPER_UPDATED = "satel_integra.zones_tamper"
+SIGNAL_MEM_TAMPER_UPDATED = "satel_integra.zones_mem_tamper"
+SIGNAL_BYPASS_UPDATED = "satel_integra.zones_bypass"
+SIGNAL_MASKED_UPDATED = "satel_integra.zones_masked"
+SIGNAL_MEM_MASKED_UPDATED = "satel_integra.zones_mem_masked"
+
 SIGNAL_OUTPUTS_UPDATED = "satel_integra.outputs_updated"
