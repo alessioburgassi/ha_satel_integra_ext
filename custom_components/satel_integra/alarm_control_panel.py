09d9b1700c1b501c0ccc2f04c24d68c1eaa0ba70
alessioburgassi <a.burgassi@gmail.com>
2025-03-21 21:12:08 +0100

    Update alarm_control_panel.py

--- custom_components/satel_integra/alarm_control_panel.py
+++ custom_components/satel_integra/alarm_control_panel.py
@@ -1,9 +1,15 @@
-"""Support for Satel Integra zone states- represented as binary sensors."""
+"""Support for Satel Integra alarm, using ETHM module."""
 from __future__ import annotations
 
-from homeassistant.components.binary_sensor import (
-    BinarySensorDeviceClass,
-    BinarySensorEntity,
+import asyncio
+from collections import OrderedDict
+
+from satel_integra2.satel_integra import AlarmState
+
+import homeassistant.components.alarm_control_panel as alarm
+from homeassistant.components.alarm_control_panel import (
+    AlarmControlPanelEntityFeature,
+    AlarmControlPanelState,
 )
 from homeassistant.core import HomeAssistant, callback
 from homeassistant.helpers.dispatcher import async_dispatcher_connect
@@ -12,13 +18,26 @@ from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
 
 from .entity import SatelIntegraEntity
 from .const import (
-    CONF_OUTPUTS,
+    CONF_ARM_HOME_MODE,
+    CONF_DEVICE_PARTITIONS,
     CONF_ZONE_NAME,
-    CONF_ZONE_TYPE,
-    CONF_ZONES,
     DATA_SATEL,
-    SIGNAL_OUTPUTS_UPDATED,
-    SIGNAL_ZONES_UPDATED
+    SIGNAL_PANEL_MESSAGE,
+    _LOGGER
+)
+
+STATE_MAP = OrderedDict(
+    [
+        (AlarmState.TRIGGERED, AlarmControlPanelState.TRIGGERED),
+        (AlarmState.TRIGGERED_FIRE, AlarmControlPanelState.TRIGGERED),
+        (AlarmState.ENTRY_TIME, AlarmControlPanelState.PENDING),
+        (AlarmState.ARMED_MODE3, AlarmControlPanelState.ARMED_HOME),
+        (AlarmState.ARMED_MODE2, AlarmControlPanelState.ARMED_HOME),
+        (AlarmState.ARMED_MODE1, AlarmControlPanelState.ARMED_HOME),
+        (AlarmState.ARMED_MODE0, AlarmControlPanelState.ARMED_AWAY),
+        (AlarmState.EXIT_COUNTDOWN_OVER_10, AlarmControlPanelState.PENDING),
+        (AlarmState.EXIT_COUNTDOWN_UNDER_10, AlarmControlPanelState.PENDING),
+    ]
 )
 
 async def async_setup_platform(
@@ -27,87 +46,118 @@ async def async_setup_platform(
     async_add_entities: AddEntitiesCallback,
     discovery_info: DiscoveryInfoType | None = None,
 ) -> None:
-    """Set up the Satel Integra binary sensor devices."""
+    """Set up for Satel Integra alarm panels."""
     if not discovery_info:
         return
 
-    configured_zones = discovery_info[CONF_ZONES]
+    configured_partitions = discovery_info[CONF_DEVICE_PARTITIONS]
     controller = hass.data[DATA_SATEL]
 
     devices = []
 
-    for zone_num, device_config_data in configured_zones.items():
-        zone_type = device_config_data[CONF_ZONE_TYPE]
+    for partition_num, device_config_data in configured_partitions.items():
         zone_name = device_config_data[CONF_ZONE_NAME]
-        device = SatelIntegraBinarySensor(
-            controller, zone_num, zone_name, zone_type, "input", SIGNAL_ZONES_UPDATED
-        )
-        devices.append(device)
-
-    configured_outputs = discovery_info[CONF_OUTPUTS]
-
-    for zone_num, device_config_data in configured_outputs.items():
-        zone_type = device_config_data[CONF_ZONE_TYPE]
-        zone_name = device_config_data[CONF_ZONE_NAME]
-        device = SatelIntegraBinarySensor(
-            controller, zone_num, zone_name, zone_type, "output", SIGNAL_OUTPUTS_UPDATED
+        arm_home_mode = device_config_data.get(CONF_ARM_HOME_MODE)
+        device = SatelIntegraAlarmPanel(
+            controller, zone_name, arm_home_mode, partition_num
         )
         devices.append(device)
 
     async_add_entities(devices)
 
+class SatelIntegraAlarmPanel(SatelIntegraEntity, alarm.AlarmControlPanelEntity):
+    """Representation of an AlarmDecoder-based alarm panel."""
 
-class SatelIntegraBinarySensor(SatelIntegraEntity, BinarySensorEntity):
-    """Representation of an Satel Integra binary sensor."""
-
+    _attr_code_format = alarm.CodeFormat.NUMBER
     _attr_should_poll = False
+    _attr_supported_features = (
+        AlarmControlPanelEntityFeature.ARM_HOME
+        | AlarmControlPanelEntityFeature.ARM_AWAY
+    )
 
-    def __init__(
-        self, controller, device_number, device_name, zone_type, device_type, react_to_signal
-    ):
-        """Initialize the binary_sensor."""
-        super().__init__(controller, device_number, device_name, device_type)
-        self._zone_type = zone_type
-        self._state = 0
-        self._react_to_signal = react_to_signal
-
+    def __init__(self, controller, name, arm_home_mode, partition_id):
+        """Initialize the alarm panel."""
+        super().__init__(controller, partition_id, name, "zone")
+        self._arm_home_mode = arm_home_mode
+        self._device_number = partition_id
+        self._satel_alarm_state = self._read_alarm_state()
+    
     async def async_added_to_hass(self) -> None:
-        """Register callbacks."""
-        if self._react_to_signal == SIGNAL_OUTPUTS_UPDATED:
-            if self._device_number in self._satel.violated_outputs:
-                self._state = 1
-            else:
-                self._state = 0
-        else:
-            if self._device_number in self._satel.violated_zones:
-                self._state = 1
-            else:
-                self._state = 0
+        """Update alarm status and register callbacks for future updates."""
+        _LOGGER.debug("Starts listening for panel messages")
         self.async_on_remove(
             async_dispatcher_connect(
-                self.hass, self._react_to_signal, self._devices_updated
+                self.hass, SIGNAL_PANEL_MESSAGE, self._update_alarm_status
             )
         )
 
-    @property
-    def icon(self):
-        """Icon for device by its type."""
-        if self._zone_type is BinarySensorDeviceClass.SMOKE:
-            return "mdi:fire"
-
-    @property
-    def is_on(self):
-        """Return true if sensor is on."""
-        return self._state == 1
-
-    @property
-    def device_class(self):
-        """Return the class of this sensor, from DEVICE_CLASSES."""
-        return self._zone_type
 
     @callback
-    def _devices_updated(self, zones):
-        """Update the zone's state, if needed."""
-        if self._device_number in zones and self._state != zones[self._device_number]:
-            self._state = zones[self._device_number]
+    def _update_alarm_status(self):
+        """Handle alarm status update."""
+        state = self._read_alarm_state()
+        if state != self._satel_alarm_state:
+            _LOGGER.debug("Partition %s CHANGED current: %s, old: %s", self._device_number,state,self._satel_alarm_state)
+            self._satel_alarm_state = state
             self.async_write_ha_state()
+        else:
+            _LOGGER.debug("Partition %s NOT CHANGED state: %s", self._device_number,state)
+            
+
+    def _read_alarm_state(self):
+        """Read current status of the alarm and translate it into HA status."""
+
+        # Default - disarmed:
+        hass_alarm_status = AlarmControlPanelState.DISARMED
+
+        if not self._satel.connected:
+            return None
+
+        _LOGGER.debug("State map of Satel: %s", self._satel.partition_states)
+
+        for satel_state, ha_state in STATE_MAP.items():
+            if (
+                satel_state in self._satel.partition_states
+                and self._device_number in self._satel.partition_states[satel_state]
+            ):
+                hass_alarm_status = ha_state
+                break
+
+        return hass_alarm_status
+
+    async def async_alarm_disarm(self, code: str | None = None) -> None:
+        """Send disarm command."""
+        if not code:
+            _LOGGER.debug("Code was empty or None")
+            return
+
+        clear_alarm_necessary = self._satel_alarm_state == AlarmControlPanelState.TRIGGERED
+
+        _LOGGER.debug("Disarming, self._satel_alarm_state: %s", self._satel_alarm_state)
+
+        await self._satel.disarm(code, [self._device_number])
+
+        if clear_alarm_necessary:
+            # Wait 1s before clearing the alarm
+            await asyncio.sleep(1)
+            await self._satel.clear_alarm(code, [self._device_number])
+
+    async def async_alarm_arm_away(self, code: str | None = None) -> None:
+        """Send arm away command."""
+        _LOGGER.debug("Arming away")
+
+        if code:
+            await self._satel.arm(code, [self._device_number])
+
+    async def async_alarm_arm_home(self, code: str | None = None) -> None:
+        """Send arm home command."""
+        _LOGGER.debug("Arming home")
+
+        if code:
+            await self._satel.arm(code, [self._device_number], self._arm_home_mode)
+
+    @property
+    def alarm_state(self) -> AlarmControlPanelState | None:
+        """Return the state of the entity."""
+        _LOGGER.debug("Getting property alarm_state: %s", self._satel_alarm_state)
+        return self._satel_alarm_state
