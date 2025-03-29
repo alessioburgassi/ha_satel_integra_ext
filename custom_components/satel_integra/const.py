7d8617397ece8f54c1614e807d045b52dadecae0
alessioburgassi <a.burgassi@gmail.com>
2025-03-29 20:44:16 +0100

    Update const.py

--- custom_components/satel_integra/const.py
+++ custom_components/satel_integra/const.py
@@ -9,12 +9,19 @@ DEFAULT_PORT = 7094
 DEFAULT_CONF_ARM_HOME_MODE = 1
 DEFAULT_DEVICE_PARTITION = 1
 DEFAULT_ZONE_TYPE = "motion"
+DEFAULT_EXPANDER_BATTERY = "no"
 DATA_SATEL = "satel_integra"
 
 CONF_DEVICE_CODE = "code"
 CONF_DEVICE_PARTITIONS = "partitions"
 CONF_ARM_HOME_MODE = "arm_home_mode"
 CONF_ZONE_NAME = "name"
+CONF_EXPANDER_BATTERY = "battery"
+CONF_EXPANDER ="expander"
+CONF_KEYPAD ="keypad"
+CONF_TROUBLE ="trouble"
+CONF_TROUBLE2 ="trouble2"
+
 CONF_ZONE_TYPE = "type"
 CONF_ZONES = "zones"
 CONF_ZONES_ALARM = "zones_alarm"
@@ -24,9 +31,11 @@ CONF_ZONES_MEM_TAMPER = "zones_mem_tamper"
 CONF_ZONES_BYPASS = "zones_bypass"
 CONF_ZONES_MASKED = "zones_masked"
 CONF_ZONES_MEM_MASKED = "zones_mem_masked"
+
 CONF_OUTPUTS = "outputs"
 CONF_TEMP_SENSORS = "temperature_sensors"
 CONF_SWITCHABLE_OUTPUTS = "switchable_outputs"
+CONF_SWITCHABLE_BYPASS = "switchable_bypass"
 CONF_INTEGRATION_KEY = "integration_key"
 CONF_TEMP_SENSOR_NAME = "name"
 ZONES = "zones"
@@ -35,7 +44,7 @@ SIGNAL_PANEL_ARM_AWAY = "satel_integra.panel_arm_away"
 SIGNAL_PANEL_ARM_HOME = "satel_integra.panel_arm_home"
 SIGNAL_PANEL_DISARM = "satel_integra.panel_disarm"
 
-SIGNAL_VIOLATED_UPDATED = "satel_integra.zones_updated"
+SIGNAL_VIOLATED_UPDATED = "satel_integra.zones_violated"
 SIGNAL_ALARM_UPDATED = "satel_integra.zones_alarm"
 SIGNAL_MEM_ALARM_UPDATED = "satel_integra.zones_mem_alarm"
 SIGNAL_TAMPER_UPDATED = "satel_integra.zones_tamper"
@@ -45,3 +54,8 @@ SIGNAL_MASKED_UPDATED = "satel_integra.zones_masked"
 SIGNAL_MEM_MASKED_UPDATED = "satel_integra.zones_mem_masked"
 
 SIGNAL_OUTPUTS_UPDATED = "satel_integra.outputs_updated"
+
+SIGNAL_OUTPUTS_BYPASS_UPDATED = "satel_integra.outputs_bypass_updated"
+
+SIGNAL_TROUBLE_UPDATED = "satel_integra.trouble_updated"
+SIGNAL_TROUBLE2_UPDATED = "satel_integra.trouble2_updated"
